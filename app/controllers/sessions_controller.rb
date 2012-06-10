454e3779170c3d12d0776c49682911034123a5a2
Sandy <Sandra.Jackiewicz@gmail.com>
2012-06-10 11:58:44 -0500

    Updated design files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_url, notice: 'See ya!'
+    redirect_to root_url
   end
   
   def new
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     if landlord
       if landlord.authenticate(params[:password])
         session[:uid] = landlord.id
-        redirect_to root_url, notice: "Welcome!"
+        redirect_to listings_url
       else
         flash[:notice] = "Unknown email or password"
         render :new
